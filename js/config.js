fbfe1a300f63b20f68d73e483b4f3aeefcea53c3
huishuais <99812266+huishuais@users.noreply.github.com>
2025-07-26 18:00:46 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -14,7 +14,7 @@ const PASSWORD_CONFIG = {
 // 网站信息配置
 const SITE_CONFIG = {
     name: 'LibreTV',
-    url: 'https://libretv.is-an.org',
+    url: 'https://itv.2video.cc/',
     description: '免费在线视频搜索与观看平台',
     logo: 'image/logo.png',
     version: '1.0.3'
