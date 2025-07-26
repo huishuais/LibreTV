5ded81b3c7b3efb91b386d7923e79125cd246eae
huishuais <99812266+huishuais@users.noreply.github.com>
2025-07-26 18:54:16 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -28,7 +28,7 @@ const API_SITES = {
         detail: 'http://caiji.dyttzyapi.com', 
     },
     ruyi: {
-        api: 'https://caiji.maotaizy.cc/api.php/provide/vod/at/josn/',
+        api: 'https://caiji.maotaizy.cc/api.php/provide/vod',
         name: '官方资源',
     },
     bfzy: {
